5c9abb0030ab529c91fe16ae74d51406809be3c2
SixFourSeven <thisemail@doesnot.exist>
2021-04-03 11:20:33 +0200

    fixed commands help

--- sharbul__cogs/User.py
+++ sharbul__cogs/User.py
@@ -13,7 +13,7 @@ class UserCommandsCog(commands.Cog):
         footer = "Sharbull Security - Developed by 647"
         icon_url = "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678094-shield-512.png"
         if page == "commands":
-            title = "About the commands",
+            title = "About the commands"
             description = "``!!setup`` : Open minimum configuration menu\n - Permission required : administrator\n\n"
             "``!!mute <Member>`` : Mute a member and report their account to the Sharbull database\n - Permission required : mute members\n\n"
             "``!!kick <Member>`` : Kick a member and report their account to the Sharbull database\n - Permission required : kick members\n\n"
